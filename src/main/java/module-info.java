e48a66e59416ea4ff0c13587adf435bad8639325
dre sac <amsaco86868@liceo.edu.ph>
2025-10-25 20:47:28 +0800

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.scheily {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.scheily to javafx.fxml;
+    exports com.example.scheily;
+}
