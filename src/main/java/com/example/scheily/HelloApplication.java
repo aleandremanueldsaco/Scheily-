840c90ad9c85d994fa1421220feb376524d0f330
dre sac <amsaco86868@liceo.edu.ph>
2025-11-10 22:26:53 +0800

    calendar

--- src/main/java/com/example/scheily/HelloApplication.java
+++ src/main/java/com/example/scheily/HelloApplication.java
@@ -1,26 +1,26 @@
-package com.example.scheily;
+    package com.example.scheily;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+    import javafx.application.Application;
+    import javafx.fxml.FXMLLoader;
+    import javafx.scene.Scene;
+    import javafx.stage.Stage;
 
-import java.io.IOException;
+    import java.io.IOException;
 
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-page.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        scene.getStylesheets().add(
-                HelloApplication.class.getResource("login-design.css").toExternalForm()
-        );
-        stage.setTitle("Scheily");
-        stage.setScene(scene);
-        stage.show();
+    public class HelloApplication extends Application {
+        @Override
+        public void start(Stage stage) throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("calendar.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            scene.getStylesheets().add(
+                    HelloApplication.class.getResource("login-design.css").toExternalForm()
+            );
+            stage.setTitle("Scheily");
+            stage.setScene(scene);
+            stage.show();
+        }
+
+        public static void main(String[] args) {
+            launch();
+        }
     }
-
-    public static void main(String[] args) {
-        launch();
-    }
-}
